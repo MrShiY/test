d6f2bda857e8797dccd9ab512ff0769feb3b9fad
shiyonglong <shiyonglong@OOOdeiMac.local>
2018-04-11 13:13:59 +0800

    tijiao666

---
+++ 666/666/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  666
+//
+//  Created by shiyonglong on 2018/4/11.
+//  Copyright © 2018年 shiyonglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 666/666/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  666
+//
+//  Created by shiyonglong on 2018/4/11.
+//  Copyright © 2018年 shiyonglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
